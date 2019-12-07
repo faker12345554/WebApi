c2e48c646549d7451feb49eb6a114375cc7af2d9
tjh <406353390@qq.com>
2019-12-07 14:28:07 +0800

    singin leave and location

---
+++ admin/src/main/java/com/admin/admin/controller/app/MessageController.java
@@ -0,0 +1,29 @@
+package com.admin.admin.controller.app;
+
+
+import com.admin.admin.service.app.MessageService;
+import com.admin.token.TokenUtil;
+import com.common.common.result.ResponseResult;
+import com.common.common.result.ResultCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Message")
+public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+    private ResponseResult result = new ResponseResult();
+    private TokenUtil tokenUtil =new TokenUtil();
+
+
+    @GetMapping("/Get")
+    public ResponseResult getNotificationList(){
+        result.setCode(ResultCode.SUCCESS.getCode());
+        result.setMessage(ResultCode.SUCCESS.getMessage());
+        return result.setData(messageService.getNotificationList());
+    }
+}
