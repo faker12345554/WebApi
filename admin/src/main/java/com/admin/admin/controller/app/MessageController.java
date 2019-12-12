6c79d1aac50870694200e0443103710692c7aba1
tjh <406353390@qq.com>
2019-12-12 09:16:58 +0800

    singin leave and location

--- admin/src/main/java/com/admin/admin/controller/app/MessageController.java
+++ admin/src/main/java/com/admin/admin/controller/app/MessageController.java
@@ -5,10 +5,14 @@ import com.admin.admin.service.app.MessageService;
 import com.admin.token.TokenUtil;
 import com.common.common.result.ResponseResult;
 import com.common.common.result.ResultCode;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/Message")
@@ -17,13 +21,12 @@ public class MessageController {
     @Autowired
     private MessageService messageService;
     private ResponseResult result = new ResponseResult();
-    private TokenUtil tokenUtil =new TokenUtil();
-
-
+    private String UserId =TokenUtil.getTokenUserId();
     @GetMapping("/Get")
-    public ResponseResult getNotificationList(){
+    public ResponseResult getNotificationList(@RequestParam(required = false) String UserId, HttpServletResponse response){
+
         result.setCode(ResultCode.SUCCESS.getCode());
         result.setMessage(ResultCode.SUCCESS.getMessage());
-        return result.setData(messageService.getNotificationList());
+        return result.setData(messageService.getNotificationList(UserId));
     }
 }
