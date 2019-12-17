1b6282b72965cfeaa7289344f8151acffcd676d5
Rookie-Fish913 <1095132588@qq.com>
2019-12-17 10:04:53 +0800

    Merge branch 'JiHao' of https://github.com/16603022157/WebApi into XueKe

---
+++ common/src/main/java/com/common/common/result/ResultSet.java
@@ -0,0 +1,33 @@
+package com.common.common.result;
+
+public class ResultSet {
+    public int resultCode;
+    public String resultMsg;
+    public Object data;
+
+    public int getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(int resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public String getResultMsg() {
+        return resultMsg;
+    }
+
+    public void setResultMsg(String resultMsg) {
+        this.resultMsg = resultMsg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+
+}
