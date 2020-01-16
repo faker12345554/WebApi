64647baf41d1702622fbda257de1b30146db594b
tjh <406353390@qq.com>
2020-01-16 15:50:09 +0800

    修改userId为personId 用手机号关联

--- prisonapp/src/main/java/com/prisonapp/business/controller/dw_supervise/SuperviseController.java
+++ prisonapp/src/main/java/com/prisonapp/business/controller/dw_supervise/SuperviseController.java
@@ -54,11 +54,11 @@ public class SuperviseController {
         if (statusCode.equals("0")) {
             ApplyLeaveListModels = superviseService.getAllApplyLeaveList(count, requestCount, getPersonId());
         } else {
-            ApplyLeaveListModels = superviseService.getApplyLeaveList(statusCode, count, requestCount, TokenUtil.getTokenUserId());
+            ApplyLeaveListModels = superviseService.getApplyLeaveList(statusCode, count, requestCount, getPersonId());
         }
         if (ApplyLeaveListModels != null && !"".equals(ApplyLeaveListModels) && !"null".equals(ApplyLeaveListModels)) {
 
-            int totalCount = (superviseService.getTotalApplyLeaveList(statusCode, TokenUtil.getTokenUserId())).size();
+            int totalCount = (superviseService.getTotalApplyLeaveList(statusCode, getPersonId())).size();
             for (GetApplyLeaveListModel item : ApplyLeaveListModels) {
                 List<ApplyRecordModel> applyRecords = superviseService.applyRecord(item.getCode());//取出请假申请
                 Long End = Long.parseLong(item.getEndTimestamp());
@@ -86,11 +86,11 @@ public class SuperviseController {
 
 //        Long longStartDate = Long.valueOf(submitApplyLeaveModel.getStartDate());//123456789
 //        Long longEndDate = Long.valueOf(submitApplyLeaveModel.getEndDate());
-        List<TPersoninformation> person = superviseService.getPersonname(TokenUtil.getTokenUserId());
+        List<TPersoninformation> person = superviseService.getPersonname(getPersonId());
         int res = superviseService.submitApplyLeave(submitApplyLeaveModel.getCity(),    submitApplyLeaveModel.getCityCode(),    submitApplyLeaveModel.getDistrict(),submitApplyLeaveModel.getDistrictCode(),
                                                     submitApplyLeaveModel.getProvince(),submitApplyLeaveModel.getProvinceCode(),submitApplyLeaveModel.getReason(),  submitApplyLeaveModel.getReasonAudioUrl(),
                                                     submitApplyLeaveModel.getEndDate(), submitApplyLeaveModel.getStartDate(),code,
-                                                    TokenUtil.getTokenUserId(), person.get(0).getPersonname());
+                                                    getPersonId(), person.get(0).getPersonname());
         if (res != 0) {
             result.resultCode = 0;
             result.resultMsg = "";
@@ -138,7 +138,7 @@ public class SuperviseController {
     public ResultSet getSuperviseTask() {
         // List<GetSuperviseTaskModel> getSuperviseTaskModel = new ArrayList<>();
         // Calendar calendar = Calendar.getInstance();
-        List<GetSuperviseTaskModel> getSuperviseTaskModels = superviseService.getSuperviseTask(TokenUtil.getTokenUserId());
+        List<GetSuperviseTaskModel> getSuperviseTaskModels = superviseService.getSuperviseTask(getPersonId());
         /*for(GetSuperviseTaskModel item: getSuperviseTaskModels){
             GetSuperviseTaskModel getSuperviseTaskModel1=new GetSuperviseTaskModel();
             getSuperviseTaskModel1.setStartDate(item.getStartDate());
@@ -164,7 +164,7 @@ public class SuperviseController {
     @ApiOperation(value = "保外人员人脸识别签到")
     @PostMapping("/faceRecognize")
     public ResultSet faceRecognize(MultipartFile file) throws Exception {
-        List<TPersoninformation> tPersoninformations = superviseService.faceRecognize(TokenUtil.getTokenUserId());
+        List<TPersoninformation> tPersoninformations = superviseService.faceRecognize(getPersonId());
         try {
             SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
             Date date = new Date(System.currentTimeMillis());
@@ -188,8 +188,8 @@ public class SuperviseController {
                     String similar = jsonObject.getString("confidence");
                     float fSimilar = Float.parseFloat(similar);
                     if (fSimilar >= 0.75) {
-                        superviseService.insertFaceRecognize(TokenUtil.getTokenUserId(), 0, 0, upLoadFaceUrl);
-                        List<FaceRecognizeModel> faceRecognizeModels = superviseService.getFaceRecognize(TokenUtil.getTokenUserId(), 0);
+                        superviseService.insertFaceRecognize(getPersonId(), 0, 0, upLoadFaceUrl);
+                        List<FaceRecognizeModel> faceRecognizeModels = superviseService.getFaceRecognize(getPersonId(), 0);
                         faceRecognizeModel.setCode(faceRecognizeModels.get(0).getCode());
                         faceRecognizeModel.setPassed(true);
                         faceRecognizeModel.setSimilar(fSimilar);
@@ -198,8 +198,8 @@ public class SuperviseController {
                         result.resultMsg = "";
                         result.data = faceRecognizeModel;
                     } else {
-                        superviseService.insertFaceRecognize(TokenUtil.getTokenUserId(), 0, 1, upLoadFaceUrl);
-                        List<FaceRecognizeModel> faceRecognizeModels = superviseService.getFaceRecognize(TokenUtil.getTokenUserId(), 0);
+                        superviseService.insertFaceRecognize(getPersonId(), 0, 1, upLoadFaceUrl);
+                        List<FaceRecognizeModel> faceRecognizeModels = superviseService.getFaceRecognize(getPersonId(), 0);
                         faceRecognizeModel.setCode(faceRecognizeModels.get(0).getCode());
                         faceRecognizeModel.setPassed(false);
                         faceRecognizeModel.setSimilar(fSimilar);
@@ -235,20 +235,20 @@ public class SuperviseController {
     public ResultSet autoLocation(@ApiParam(name = "latitude", value = "纬度") @RequestParam(required = true) float latitude, @ApiParam(name = "longitude", value = "经度") @RequestParam(required = true) float longitude, @ApiParam(name = "locationType", value = "定位类型") @RequestParam(required = true) int locationType, @ApiParam(name = "address", value = "地址") @RequestParam(required = true) String address) throws MalformedURLException {
        boolean fScope = getPolygon(latitude,longitude);
         if (fScope) {
-          //  superviseService.updateFscope(TokenUtil.getTokenUserId(), false);
+          //  superviseService.updateFscope(getPersonId(), false);
 
         } else {
             //生成报警内容
-            String persionName = superviseService.faceRecognize(TokenUtil.getTokenUserId()).get(0).getPersonname();
+            String persionName = superviseService.faceRecognize(getPersonId()).get(0).getPersonname();
             Date now = new Date();
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//可以方便地修改日期格式
             String nowTime = dateFormat.format(now);
             String content = persionName + "于" + nowTime + "未经批准离开限定区域，请及时与其取得联系。最后一次位置：";
-         //   superviseService.updateFscope(TokenUtil.getTokenUserId(), true);
-            superviseService.insertFscope(TokenUtil.getTokenUserId(), content);
+         //   superviseService.updateFscope(getPersonId(), true);
+            superviseService.insertFscope(getPersonId(), content);
 
         }
-        int a = superviseService.autoLocation(latitude, longitude, locationType, address, TokenUtil.getTokenUserId(), new Date(),fScope);
+        int a = superviseService.autoLocation(latitude, longitude, locationType, address, getPersonId(), new Date(),fScope);
         if (a != 0) {
             result.resultCode = 0;
             result.resultMsg = "";
@@ -266,7 +266,7 @@ public class SuperviseController {
     @PostMapping("/uploadLocationError")
     public ResultSet uploadLocationError(String errorCode, String errorMsg) {
 
-        int a = superviseService.uploadLocationError(errorCode, errorMsg, Integer.parseInt(TokenUtil.getTokenUserId()), new Date());
+        int a = superviseService.uploadLocationError(errorCode, errorMsg, Integer.parseInt(getPersonId()), new Date());
         if (a != 0) {
             result.resultCode = 0;
             result.resultMsg = "";
@@ -284,9 +284,9 @@ public class SuperviseController {
     @PostMapping("/uploadBattery")
     public ResultSet uploadBattery(float percent) {
         if (percent <= 20.0) {
-            batteryAlarm(TokenUtil.getTokenUserId());
+            batteryAlarm(getPersonId());
         }
-        int a = superviseService.uploadBattery(percent, TokenUtil.getTokenUserId(), new Date());
+        int a = superviseService.uploadBattery(percent, getPersonId(), new Date());
         result.resultCode = 0;
         result.resultMsg = "";
         result.data = new Object();
@@ -316,7 +316,7 @@ public class SuperviseController {
         battery.setEnable(true);
         battery.setTimeSpan("20");
         battery.setAlarmThreshold(20.0f);
-        GetSuperviseConfigModel getSuperviseConfigModel = new GetSuperviseConfigModel(); // = superviseService.getBatteryConfigTimestamp(TokenUtil.getTokenUserId());
+        GetSuperviseConfigModel getSuperviseConfigModel = new GetSuperviseConfigModel(); // = superviseService.getBatteryConfigTimestamp(getPersonId());
         getSuperviseConfigModel.setLocation(locationModels);
         getSuperviseConfigModel.setBattery(battery);
         //最后时间
@@ -341,7 +341,7 @@ public class SuperviseController {
             Point2D.Double point = new Point2D.Double(dLatitude, dLongitude);
             //画区域
             List<Point2D.Double> polygon = new ArrayList<Point2D.Double>();
-            TEnclosure tEnclosure = superviseService.getPolygon(TokenUtil.getTokenUserId());
+            TEnclosure tEnclosure = superviseService.getPolygon(getPersonId());
             if (tEnclosure.getAreaarr() == null || "".equals(tEnclosure.getAreaarr())) {//数据库中没有坐标，只有地方名的情况
                 String path = "https://restapi.amap.com/v3/config/district?key=f0bc84013740494ba5c697ce6b707606&keywords=" + tEnclosure.getAreaname() + "&subdistrict=0&extensions=all";
                 net.sf.json.JSONObject josnResult = AddressResolutionUtil.getHttps(path);//高德api返回的结果集
@@ -372,20 +372,20 @@ public class SuperviseController {
             }
              fScope = checkWithJdkGeneralPath(point, polygon);//点是否在区域内，则返回true时不越界，反之越界
    /*         if (a) {
-                superviseService.updateFscope(TokenUtil.getTokenUserId(), false);
+                superviseService.updateFscope(getPersonId(), false);
                 result.resultCode = 0;
                 result.resultMsg = "没有越界";
                 result.data = new Object();
             } else {
                 //生成报警内容
-                String persionName = superviseService.faceRecognize(TokenUtil.getTokenUserId()).get(0).getPersonname();
+                String persionName = superviseService.faceRecognize(getPersonId()).get(0).getPersonname();
                 Date now = new Date();
                 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//可以方便地修改日期格式
                 String nowTime = dateFormat.format(now);
                 String content = persionName + "于" + nowTime + "未经批准离开限定区域，请及时与其取得联系。最后一次位置：";
 
-                superviseService.updateFscope(TokenUtil.getTokenUserId(), true);
-                superviseService.insertFscope(TokenUtil.getTokenUserId(), content);
+                superviseService.updateFscope(getPersonId(), true);
+                superviseService.insertFscope(getPersonId(), content);
                 result.resultCode = 0;
                 result.resultMsg = "越界了";
                 result.data = new Object();
