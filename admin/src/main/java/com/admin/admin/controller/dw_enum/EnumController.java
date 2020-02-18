0c24f1b6f24c7efbe82625382a62b73b51e1f6a1
dell <1095132588@qq.com>
2020-02-18 16:08:19 +0800

    修改bug

--- admin/src/main/java/com/admin/admin/controller/dw_enum/EnumController.java
+++ admin/src/main/java/com/admin/admin/controller/dw_enum/EnumController.java
@@ -5,11 +5,10 @@ import com.admin.token.tation.UserLoginToken;
 import com.common.common.result.ResponseResult;
 import com.common.common.result.ResultCode;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Api(value="基础数据controller",tags={"基础数据"})
