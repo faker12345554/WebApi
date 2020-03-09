869ed961c9a8e613fb9cfcf1f413046ea180bc9f
dell <1095132588@qq.com>
2020-03-09 10:52:31 +0800

    上传照片

--- admin/src/main/java/com/admin/admin/controller/dw_picture/PictureReportController.java
+++ admin/src/main/java/com/admin/admin/controller/dw_picture/PictureReportController.java
@@ -2,19 +2,25 @@ package com.admin.admin.controller.dw_picture;
 
 import com.admin.admin.entity.dw_person.Personinformation;
 import com.admin.admin.service.dw_picture.PictureReportService;
+import com.admin.token.tation.PassToken;
 import com.admin.token.tation.UserLoginToken;
+import com.common.common.Uploadfiles.Upload;
 import com.common.common.result.ResponseResult;
 import com.common.common.result.ResultCode;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.aspectj.util.FileUtil.copyDir;
 import static org.aspectj.util.FileUtil.copyFile;
@@ -25,7 +31,7 @@ public class PictureReportController {
     @Autowired
     private PictureReportService pictureReportService;
 
-
+    private Upload upload=new Upload();
 
     @UserLoginToken
     @ApiOperation(value = "人脸识别数据导入")
@@ -60,5 +66,26 @@ public class PictureReportController {
             result.setMessage(ResultCode.NO_PERSONID.getMessage());
             return result.setData(null);
         }
+    }
+
+    @PassToken
+    @ApiOperation(value = "上传照片")
+    @PostMapping("/uploadPicture")
+    public ResponseResult uploadPicture(@ApiParam(name = "picture",value = "照片")@RequestParam(required =true) MultipartFile picture){
+        ResponseResult result = new ResponseResult();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date(System.currentTimeMillis());
+        String url = System.getProperty("user.dir") + "/../webapps/mypicture/admin/" + formatter.format(date)+"/";
+        File path = new File(url);
+        if (!path.exists() && !path.isDirectory()) {
+            path.setWritable(true,false);
+            path.mkdirs();
+        }
+        String res = upload.upload(url, picture);
+        String fileName = picture.getOriginalFilename();
+        String filePath="https://pardon.cnnc626.com:8443/mypicture/admin/"+ formatter.format(date)+"/"+fileName;
+        result.setCode(ResultCode.SUCCESS.getCode());
+        result.setMessage(ResultCode.SUCCESS.getMessage());
+        return result.setData(filePath);
     }
 }
