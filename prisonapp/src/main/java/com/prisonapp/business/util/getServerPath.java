43245fd7d1023959fb6d2c30d3a6c880f28d2516
tjh <406353390@qq.com>
2020-03-24 11:11:00 +0800

    Merge branch 'master' of https://github.com/16603022157/WebApi into JiHao
     Conflicts:
    	admin/src/main/resources/mapper/user/UserMapper.xml

--- prisonapp/src/main/java/com/prisonapp/business/util/getServerPath.java
+++ prisonapp/src/main/java/com/prisonapp/business/util/getServerPath.java
@@ -5,7 +5,7 @@ import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 public class getServerPath {
     public static String ServerPath(){
 //        String ServerPath ="http://68.190.202.163:8000/mypicture/personApp/";
-        String ServerPath ="http://pardon.cnnc626.com:8443/mypicture/personApp/";
+        String ServerPath ="https://pardon.cnnc626.com:8443/mypicture/personApp/";
         return ServerPath;
     }
 }
